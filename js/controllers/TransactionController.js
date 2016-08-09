9be8794a76e513c58f59092fb86f52b597915e2b
Diego Lira <lira@dynamix.com.br>
2016-08-09 12:54:33 -0300

    Passagem da tela de transactions para angular
    Ajuste na tela de configuração para funcionar com tab

--- js/controllers/TransactionController.js
+++ js/controllers/TransactionController.js
@@ -1,4 +1,55 @@
 angular.module('dynas')
-    .controller('TransactionController', ['$scope', function() {
+    .controller('TransactionController', ['$scope', '$uibModal', 'blockUI', 'TransactionService',
+        function ($scope, $uibModal, blockUI, TransactionService) {
 
-}]);
+            var panelTransactionBlock = blockUI.instances.get('panel-transactions');
+
+            //Filtros
+            $scope.showFilter = false;
+            $scope.groups = [];
+            $scope.filter = {};
+
+            $scope.changeFilterVisibility = function () {
+                $scope.showFilter = !$scope.showFilter;
+            };
+
+            $scope.query = function () {
+
+                panelTransactionBlock.start();
+
+                var promisePage = TransactionService.query($scope.filter, $scope.currentPage);
+
+                promisePage
+                    .then(function (page) {
+                        $scope.currentPage = page.currentPage;
+                        $scope.totalItems = page.totalItems;
+                        $scope.transactions = page.items;
+                        $scope.totalTransactions = page.totalizer.value;
+
+                        panelTransactionBlock.stop();
+                        //TODO: Notificar sucesso
+                    })
+                    .catch(function () {
+                        //TODO: Tratar erros
+                    });
+            };
+
+            $scope.limparFiltros = function () {
+                $scope.filter = {};
+                $scope.currentPage = 1;
+
+                $scope.query();
+            };
+
+            //Paginacao
+            $scope.currentPage = 1;
+            $scope.totalItems = 0;
+            $scope.transactions = [];
+            $scope.totalTransactions = 0;
+
+            //Chamadas de criação de controller
+
+            //Inicializaço dos sprints
+            $scope.query();
+
+        }]);
---
+++ js/service/TransactionService.js
@@ -0,0 +1,132 @@
+angular.module('dynas')
+    .factory('TransactionService', ['$q', function ($q) {
+        var service = {};
+
+        const regPerPage = 20;
+
+        var dummyValue = {
+            currentPage: 1,
+            totalItems: 2,
+            items: [
+                {
+                    from: {
+                        id: 1,
+                        email: 'dal.lira@gmail.com',
+                        name: 'Diego',
+                        admin: false,
+                        group: {
+                            id: 1,
+                            name: 'Relacionamento'
+                        },
+                        active: true
+                    },
+                    to: {
+                        id: 2,
+                        email: 'dal_lira@hotmail.com',
+                        name: 'André',
+                        admin: false,
+                        group: {
+                            id: 1,
+                            name: 'Relacionamento'
+                        },
+                        active: true
+                    },
+                    value: 100,
+                    date: new Date(),
+                    sprint: {
+                        id: 1,
+                        name: 'Stairway to Heaven',
+                        begin: new Date(),
+                        end: new Date(),
+                        group: {
+                            id: 1,
+                            name: 'Relacionamento'
+                        },
+                        value: '1000'
+                    },
+                    motive: "Motivo"
+                },
+                {
+                    from: {
+                        id: 1,
+                        email: 'dal.lira@gmail.com',
+                        name: 'Diego',
+                        admin: false,
+                        group: {
+                            id: 1,
+                            name: 'Relacionamento'
+                        },
+                        active: true
+                    },
+                    to: {
+                        id: 2,
+                        email: 'dal_lira@hotmail.com',
+                        name: 'André',
+                        admin: false,
+                        group: {
+                            id: 1,
+                            name: 'Relacionamento'
+                        },
+                        active: true
+                    },
+                    value: 1000,
+                    date: new Date(),
+                    sprint: {
+                        id: 1,
+                        name: 'Stairway to Heaven',
+                        begin: new Date(),
+                        end: new Date(),
+                        group: {
+                            id: 1,
+                            name: 'Relacionamento'
+                        },
+                        value: '1000'
+                    },
+                    motive: "Motivo"
+                }
+            ],
+            totalizer: {
+                value: 1100
+            }
+        };
+
+        service.query = function (filter, page) {
+            var def = $q.defer();
+
+            setTimeout(function () {
+                def.resolve(dummyValue);
+            }, 2000);
+
+            return def.promise;
+        };
+
+        service.create = function (transaction) {
+            var def = $q.defer();
+
+            setTimeout(function () {
+                dummyValue.items.push(transaction);
+                dummyValue.totalItems = dummyValue.totalItems + 1;
+
+                def.resolve();
+            }, 2000);
+
+            return def.promise;
+        };
+
+        service.save = function (sprint) {
+            var def = $q.defer();
+
+            setTimeout(function () {
+                var index = dummyValue.items.findIndex(function (s) {
+                    return s.id === sprint.id;
+                });
+                dummyValue.items[index] = sprint;
+
+                def.resolve();
+            }, 2000);
+
+            return def.promise;
+        };
+
+        return service;
+    }]);
