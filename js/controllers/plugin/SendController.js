1b0bdff85354dd3a9436e38ea491ebbc166b2cd8
Diego Lira <lira@dynamix.com.br>
2016-09-15 12:45:57 -0300

    Ajuste no envio de transações via plugin

--- js/controllers/plugin/SendController.js
+++ js/controllers/plugin/SendController.js
@@ -1,11 +1,9 @@
-app.controller('SendController', ['$scope', 'RouteService', 'UserService', 'LoginService',
-    function ($scope, RouteService, UserService, LoginService) {
+app.controller('SendController', ['$scope', 'RouteService', 'UserService', 'LoginService', 'TransactionService', 'blockUI',
+    function ($scope, RouteService, UserService, LoginService, TransactionService, blockUI) {
 
         LoginService.getCurrentUser()
             .then(function (user) {
-
                 $scope.identity = user;
-                $scope.transaction.from = user;
 
                 return UserService.getFromGroup(user.group);
             }).then(function (users) {
@@ -37,7 +35,22 @@ app.controller('SendController', ['$scope', 'RouteService', 'UserService', 'Logi
         };
 
         $scope.send = function () {
-            console.log($scope.transaction);
+            if (!$scope.sendForm.$valid) {
+                return;
+            }
+
+            blockUI.start();
+            TransactionService.create($scope.transaction)
+                .then(function () {
+                    RouteService.toMainScreen();
+                })
+                .catch(function (err) {
+                    //TODO: Mostrar erro
+                    console.log(err);
+                })
+                .finally(function () {
+                    blockUI.stop();
+                });
         };
 
         $scope.cancel = function () {
